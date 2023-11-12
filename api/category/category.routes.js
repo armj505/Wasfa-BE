e3a2185156814d82abde500b61754eed0768d6df
thuwainik <142627387+thuwainik@users.noreply.github.com>
2023-11-12 19:43:04 +0300

    test

--- api/category/category.routes.js
+++ api/category/category.routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.get();
 router.post("/category", createCategory);
+router.put("/category/:categoryId", updtCategory);
+router.delete("category/:categoryId", delCategory);
 module.exports = router;
